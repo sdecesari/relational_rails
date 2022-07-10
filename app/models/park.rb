ed738e6fc79b14726395164402353be98ef40ce9
Shirley DeCesari <shirley.decesari@gmail.com>
2022-07-10 12:26:46 -0700

    User story 8 and 9 - Add links to parent and child index

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,5 +1,5 @@
 class Park < ApplicationRecord
-   has_many :trails
+   has_many :trails, dependent: :destroy
    validates_presence_of :name
    validates_presence_of :daily_visitors
    validates :cost, inclusion: [true, false]
--- spec/features/trails/index_spec.rb
+++ spec/features/trails/index_spec.rb
@@ -8,6 +8,26 @@ RSpec.describe 'index page view' do
    visit '/trails'
 
    expect(page).to have_content(trail.name)
-   # save_and_open_page
+end 
+
+   it "can take you back to trail index" do 
+      park = Park.create!(name: "Yosimite National Park", cost: true, daily_visitors: 39839)
+      trail = park.trails.create!(name: "Cook's Meadow Trail", difficulty: 1, open: true) 
+      
+      visit '/trails'
+      click_link 'Trails'
+      
+      expect(page).to have_link("Trails", :href=>"/trails")
    end 
+
+   it "can take you back to park index" do 
+      park = Park.create!(name: "Yosimite National Park", cost: true, daily_visitors: 39839)
+      trail = park.trails.create!(name: "Cook's Meadow Trail", difficulty: 1, open: true) 
+
+      visit '/trails'
+      click_link 'Parks'
+      
+      expect(page).to have_link("Parks", :href=>"/parks")
+   # save_and_open_page
+   end
 end 
