f562965dbb0fb6c01adf84a2ea71990c5702c254
Shirley DeCesari <shirley.decesari@gmail.com>
2022-07-09 22:55:41 -0700

    User story 8 and 9 - Add links to parent and child index

--- spec/features/parks/index_spec.rb
+++ spec/features/parks/index_spec.rb
@@ -2,21 +2,47 @@ require 'rails_helper'
 
 RSpec.describe 'index page view', type: :feature do
    it "when i visit the index page i can see the name of the park" do 
-      park = Park.create!(name: "Yosimite National Park", cost: true, daily_visitors: 39839)
+      Park.destroy_all
+      park_1 = Park.create!(name: "Yosimite National Park", cost: true, daily_visitors: 39839)
+      park_2 = Park.create!(name: "Yellowstone National Park", cost: true, daily_visitors: 8923)
       
       visit '/parks'
 
-      expect(page).to have_content(park.name)
-   save_and_open_page
+      expect(page).to have_content(park_1.name)
+      expect(page).to have_content(park_2.name)
    end 
-
+   
    it "can show park created most recently" do 
+      Park.destroy_all
       park_1 = Park.create!(name: "Yosimite National Park", cost: true, daily_visitors: 39839)
       park_2 = Park.create!(name: "Yellowstone National Park", cost: true, daily_visitors: 8923)
-
+      
       visit "/parks"
-
+      
       expect(page).to have_content(park_1.created_at)
       expect(page).to have_content(park_2.created_at)
    end 
+   
+   it "can take you back to park index" do 
+      Park.destroy_all
+      park_1 = Park.create!(name: "Yosimite National Park", cost: true, daily_visitors: 39839)
+      park_2 = Park.create!(name: "Yellowstone National Park", cost: true, daily_visitors: 8923)
+      
+      visit '/parks'
+      click_link "Parks"
+      
+      expect(page).to have_link("Parks", :href=>"/parks")
+   end
+
+   it "can take you back to trail index" do 
+      Park.destroy_all
+      park_1 = Park.create!(name: "Yosimite National Park", cost: true, daily_visitors: 39839)
+      park_2 = Park.create!(name: "Yellowstone National Park", cost: true, daily_visitors: 8923)
+      
+      visit '/parks'
+      click_link "Trails"
+      
+      expect(page).to have_link("Trails", :href=>"/trails")
+      # save_and_open_page
+   end
 end 
