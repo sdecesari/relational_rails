ce161f7262c1ef87a27d234d99bf3fbfb4186580
Shirley DeCesari <shirley.decesari@gmail.com>
2022-07-12 11:03:28 -0700

    User story 12 -Add update parent link

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get '/parks', to: 'parks#index'
   get 'parks/new', to: 'parks#new'
   post 'parks', to: 'parks#create'
+  get '/parks/:id/edit', to: 'parks#edit'
+  patch '/parks/:id', to: 'parks#update'
   get '/parks/:id', to: 'parks#show'
   get '/parks/:park_id/trails', to: 'park_trails#index'
   get '/trails', to: 'trails#index'
