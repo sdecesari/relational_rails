4ce2fbf1368d3ce5c4ef1091495b1e719bd5131e
Shirley DeCesari <shirley.decesari@gmail.com>
2022-07-12 10:18:18 -0700

    User story 11 - Add parent creation

---
+++ spec/features/parks/new_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'the park creation', type: :feature do
+  it "User story 11: links to the new page from the park index" do
+    visit '/parks'
+    click_link 'New Park'
+
+    expect(current_path).to eq('/parks/new')
+  end
+
+  it "User story 11: can create a new park" do
+    visit 'parks/new'
+    fill_in('Name', with: "Zion National Park")
+    fill_in('Cost', with: true)
+    fill_in('daily_visitors', with: 98399)
+    click_button 'Create Park'
+
+    expect(current_path).to eq("/parks")
+    expect(page).to have_content("")
+  end
+
+end
